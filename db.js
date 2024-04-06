838478ff99db766029b4f3d64f57a9352583a28a
Shmiddty <corey.wade.hart@gmail.com>
2024-04-06 15:01:28 -0600

    new pages for item categories and rarities. also hides table columns when not relevant

--- db.js
+++ db.js
@@ -28,7 +28,7 @@ module.exports = function (data) {
     $types:=$split($substring(columns[name="type"].typeStr,2),",");
     $rarities:=$split($substring(columns[name="rarity"].typeStr,2),",");
     $stats:=$$.sheets[name="stats"].lines;
-    $eqByType:=[lines[active=true].(
+    [lines[active=true].(
       $i:=$;
       $ ~> | $ | {
         "type": $types[$i.type],
@@ -38,8 +38,46 @@ module.exports = function (data) {
           $merge([$mine, $me])
         )]
       }, ["attacks", "power_attacks"]|
-    )]{ type: $ };
-    $eqByType
+    )]{ type: [$] }
+  )`).evaluate(data)
+    },
+    equipmentByCategory: function () {
+      return $(`
+  $eq:=sheets[name="equipment"].(
+    $types:=$split($substring(columns[name="type"].typeStr,2),",");
+    $rarities:=$split($substring(columns[name="rarity"].typeStr,2),",");
+    $stats:=$$.sheets[name="stats"].lines;
+    [lines[active=true].(
+      $i:=$;
+      $ ~> | $ | {
+        "type": $types[$i.type],
+        "properties": [properties.(
+          $me:=$;
+          $mine:=$stats[id=$me.stat];
+          $merge([$mine, $me])
+        )]
+      }, ["attacks", "power_attacks"]|
+    )]{ category: [$] }
+  )`).evaluate(data)
+    },
+    equipmentByRarity: function () {
+      return $(`
+  $eq:=sheets[name="equipment"].(
+    $types:=$split($substring(columns[name="type"].typeStr,2),",");
+    $rarities:=$split($substring(columns[name="rarity"].typeStr,2),",");
+    $stats:=$$.sheets[name="stats"].lines;
+    [lines[active=true].(
+      $i:=$;
+      $ ~> | $ | {
+        "type": $types[$i.type],
+        "rarity": $rarities[$i.rarity],
+        "properties": [properties.(
+          $me:=$;
+          $mine:=$stats[id=$me.stat];
+          $merge([$mine, $me])
+        )]
+      }, ["attacks", "power_attacks"]|
+    )]{ rarity: [$] }
   )`).evaluate(data)
     },
     equipmentSchema: function () {
